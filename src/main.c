92090885942739a9625963ed35741f2bff3ee529
Stephen Molloy <stephen.molloy@maxiv.lu.se>
2023-12-28 00:07:41 +0100

    Style

--- src/main.c
+++ src/main.c
@@ -9,25 +9,24 @@
 #define FONTSIZE 64
 #define BACKGROUNDCOLOUR CLITERAL(Color){ 30, 30, 30, 255 }
 
-Font AddNewCharsToFont(Font font, char *filePath, char *new_chars) {
+void AddNewCharsToFontEx(Font *font, const char *fileName, int fontSize, char *newChars)
+{
   int codepointCount = 0;
-  int *codepoints = LoadCodepoints(new_chars, &codepointCount);
+  int *codepoints = LoadCodepoints(newChars, &codepointCount);
 
-  int newCount = font.glyphCount + codepointCount;
+  int newCount = font->glyphCount + codepointCount;
 
   int *newCodepoints = MemAlloc(newCount * sizeof(int));
-  for (int i=0; i<font.glyphCount; i++) {
-    newCodepoints[i] = font.glyphs[i].value;
+  for (int i=0; i<font->glyphCount; i++) {
+    newCodepoints[i] = font->glyphs[i].value;
   }
-  for (int i=font.glyphCount; i<newCount; i++) {
-    newCodepoints[i] = codepoints[i-font.glyphCount];
+  for (int i=font->glyphCount; i<newCount; i++) {
+    newCodepoints[i] = codepoints[i-font->glyphCount];
   }
 
-  Font newFont = LoadFontEx(filePath, FONTSIZE, newCodepoints, newCount);
+  *font = LoadFontEx(fileName, fontSize, newCodepoints, newCount);
 
   MemFree(newCodepoints);
-
-  return newFont;
 }
 
 int main(void) {
@@ -38,19 +37,15 @@ int main(void) {
 
   char *text = "abcdefghijklmnopqrstuvwxyzåäö";
 
-  Font new_font = AddNewCharsToFont(font, font_file_path, "öäå");
+  AddNewCharsToFontEx(&font, font_file_path, FONTSIZE, "öäå");
 
   while (!WindowShouldClose()) {
     BeginDrawing();
     ClearBackground(BACKGROUNDCOLOUR);
 
-    Vector2 broken_text_size = MeasureTextEx(font, text, FONTSIZE, 0.5);
-    Vector2 broken_centre = { .x = WIDTH/2.0 - broken_text_size.x/2.0, .y = HEIGHT/2.0 - 3*broken_text_size.y/2.0 };
-    DrawTextEx(font, text, broken_centre, FONTSIZE, 0, YELLOW);
-
-    Vector2 fixed_text_size = MeasureTextEx(new_font, text, FONTSIZE, 0.5);
-    Vector2 fixed_centre = { .x = WIDTH/2.0 - fixed_text_size.x/2.0, .y = HEIGHT/2.0 - fixed_text_size.y/2.0 };
-    DrawTextEx(new_font, text, fixed_centre, FONTSIZE, 0, YELLOW);
+    Vector2 text_size = MeasureTextEx(font, text, FONTSIZE, 0.5);
+    Vector2 centre = { .x = WIDTH/2.0 - text_size.x/2.0, .y = HEIGHT/2.0 - text_size.y/2.0 };
+    DrawTextEx(font, text, centre, FONTSIZE, 0, YELLOW);
 
     EndDrawing();
   }
