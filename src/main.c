78fef1bdbfd6c422cc18534c9f4b688e277b05c2
Stephen Molloy <stephen.molloy@maxiv.lu.se>
2023-12-28 01:21:21 +0100

    No dupes

--- src/main.c
+++ src/main.c
@@ -11,20 +11,47 @@
 
 void AddNewCharsToFontEx(Font *font, const char *fileName, int fontSize, char *newChars)
 {
+  // Get the codepoints of the newChars
   int codepointCount = 0;
   int *codepoints = LoadCodepoints(newChars, &codepointCount);
 
+  // Maximum value of the number of codepoints in the updated Font
   int newCount = font->glyphCount + codepointCount;
 
+  // Allocate space for the new codepoints and populate it
   int *newCodepoints = MemAlloc(newCount * sizeof(int));
-  for (int i=0; i<font->glyphCount; i++) {
+
+  int codepointCountWithNoDupes = 0;  // We don't need to assign chars that already exist in the Font
+  for (int i = 0; i < font->glyphCount; i++)
+  {
     newCodepoints[i] = font->glyphs[i].value;
-  }
-  for (int i=font->glyphCount; i<newCount; i++) {
-    newCodepoints[i] = codepoints[i-font->glyphCount];
+    codepointCountWithNoDupes += 1;
   }
 
-  *font = LoadFontEx(fileName, fontSize, newCodepoints, newCount);
+  for (int i = font->glyphCount; i < newCount; i++)
+  {
+    bool codepointIsUnique = true;
+    int cp = codepoints[i-font->glyphCount];
+
+    // This loop checks for the existence for the existence of the newChars in the input Font
+    for (int j = 0; j < font->glyphCount; j++)
+    {
+      if (cp == font->glyphs[j].value)
+      {
+        codepointIsUnique = false;
+        break;
+      }
+    }
+    if (codepointIsUnique)
+    {
+      newCodepoints[codepointCountWithNoDupes] = cp;
+      codepointCountWithNoDupes += 1;
+    }
+  }
+
+  // Unload the Font and then reassign the newly built Font
+  UnloadFont(*font);
+  *font = LoadFontEx(fileName, fontSize, newCodepoints, codepointCountWithNoDupes);
 
   MemFree(newCodepoints);
 }
@@ -39,7 +66,7 @@ int main(void) {
 
   char *text = "• A bulleted list\n• To demo the new function";
 
-  AddNewCharsToFontEx(&font, font_file_path, FONTSIZE, "•");
+  AddNewCharsToFontEx(&font, font_file_path, FONTSIZE, "ab•c");
 
   while (!WindowShouldClose()) {
     BeginDrawing();
